e75ec95bc5170bfce57c902a26f827861f441f45
Max Lapides <max@maxlapides.com>
2019-01-09 21:48:37 -0800

    post listings styles

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,30 +4,27 @@ import { Link, graphql } from 'gatsby'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { posts, title } = this.props
+    const { posts } = this.props
 
     return (
       <section className="section">
         <div className="container">
-          <div className="content">
+          {/* <div className="content">
             <h1 className="has-text-weight-bold is-size-2">{title}</h1>
-          </div>
+          </div> */}
           {posts.map(({ node: post }) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
+            <div className="content post-listing" key={post.id}>
               <p>
                 <Link className="has-text-primary" to={post.slug}>
                   {post.title}
                 </Link>
                 <span> &bull; </span>
                 <small>
-                  {post.date} - posted by{' '}
+                  {post.date}
+                  {/* - posted by{' '}
                   <Link to={`/author/${post.author.slug}`}>
                     {post.author.name}
-                  </Link>
+                  </Link> */}
                 </small>
               </p>
               <div>
@@ -36,9 +33,6 @@ export default class IndexPage extends React.Component {
                     __html: post.excerpt.replace(/<p class="link-more.*/, ''),
                   }}
                 />
-                <Link className="button is-small" to={post.slug}>
-                  Keep Reading →
-                </Link>
               </div>
             </div>
           ))}
