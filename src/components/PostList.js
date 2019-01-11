65612b7f49ada77e35b090be41408ed81174e77f
Max Lapides <max@maxlapides.com>
2019-01-11 09:58:05 -0800

    support portrait photos on post listings

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -53,6 +53,7 @@ export default class IndexPage extends React.Component {
                       </div>
                     </div>
                   </div>
+                  <div class="post-listing__clear" />
                 </Link>
               </div>
             )
