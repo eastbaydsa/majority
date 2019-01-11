cf3604fdaf5357e149809b84f8fb553afef54a9c
Max Lapides <max@maxlapides.com>
2019-01-11 10:23:07 -0800

    email newsletter signup endpoint

---
+++ functions-src/email-signup.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+
+const endpoint =
+  'https://eastbaydsa-staging.herokuapp.com/external/signup-majority.json'
+
+success = callback => {
+  callback(null, {
+    statusCode: 200,
+    body: 'Success',
+  })
+}
+
+error = callback => {
+  callback(null, {
+    statusCode: 500,
+    body: 'Error',
+  })
+}
+
+exports.handler = ({ body }, context, callback) => {
+  const { email, firstName, lastName } = JSON.parse(body)
+
+  const payload = {
+    email: email,
+    first_name: firstName,
+    last_name: lastName,
+  }
+
+  axios
+    .post(endpoint, payload)
+    .then(res =>
+      res.status >= 200 && res.status < 300
+        ? success(callback)
+        : error(callback)
+    )
+    .catch(() => error(callback))
+}
