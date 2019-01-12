fb529072e81be7680c0222faa334012ca2746ec6
Max Lapides <max@maxlapides.com>
2019-01-11 17:05:55 -0800

    email signup form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import axios from 'axios'
+
 import './footer.scss'
 import Logo from '../Logo'
 import ebdsaLogo from '../../img/ebdsa-logo@2x.png'
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+    fontFamily: ['ManifoldDSA', 'Helvetica Neue', 'Arial', 'Sans-Serif'].join(
+      ','
+    ),
+  },
+  palette: {
+    type: 'dark',
+  },
+})
 
 class Footer extends Component {
   render() {
@@ -9,7 +27,9 @@ class Footer extends Component {
       <footer className="footer">
         <div className="footer__content">
           <div className="footer__columns">
-            <div className="footer__column footer__column--links" />
+            <div className="footer__column footer__column--links">
+              <NewsletterSignup />
+            </div>
             <div className="footer__column footer__column--logos">
               <Logo />
               <div className="ebdsa">
@@ -29,5 +49,115 @@ class Footer extends Component {
     )
   }
 }
+
+class NewsletterSignup extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    loading: false,
+    response: null,
+  }
+
+  handleChange = fieldName => event => {
+    this.setState({ [fieldName]: event.target.value })
+  }
+
+  submit = async () => {
+    this.setState({ loading: true })
+    const res = await axios.post('/.netlify/functions/email-signup', {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+    })
+    this.setState({ loading: false, response: res.data })
+  }
+
+  render() {
+    return (
+      <div className="newsletter-signup">
+        <h3>Sign up for Majority Newsletter</h3>
+        <MuiThemeProvider theme={theme}>
+          <div className="newsletter-signup__name">
+            <Input
+              onChange={this.handleChange('firstName')}
+              value={this.state.firstName}
+              label="First Name"
+            />
+            <Input
+              onChange={this.handleChange('lastName')}
+              value={this.state.lastName}
+              label="Last Name"
+            />
+          </div>
+          <div className="newsletter-signup__email">
+            <Input
+              onChange={this.handleChange('email')}
+              value={this.state.email}
+              label="Email Address"
+            />
+          </div>
+          <div className="newsletter-signup__button-wrapper">
+            {!this.state.loading && this.state.response === null && (
+              <Button
+                onClick={this.submit}
+                disabled={
+                  this.state.email === '' ||
+                  !this.state.email.includes('@') ||
+                  !this.state.email.includes('.')
+                }
+                variant="contained"
+                className="newsletter-signup__button"
+                classes={{ disabled: 'newsletter-signup__button--disabled' }}
+              >
+                Sign Up
+              </Button>
+            )}
+            {this.state.loading && (
+              <CircularProgress
+                size={31}
+                className="newsletter-signup__progress"
+                classes={{ svg: 'newsletter-signup__progress-svg' }}
+              />
+            )}
+            {this.state.response && (
+              <p className="newsletter-signup__message">
+                {this.state.response}
+              </p>
+            )}
+            <p className="newsletter-signup__fine-print">
+              We will never sell or share your data
+            </p>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
+
+const Input = ({ onChange, value, label }) => (
+  <TextField
+    classes={{
+      root: 'footer-input',
+    }}
+    InputProps={{
+      classes: {
+        focused: 'footer-input--focused',
+        notchedOutline: 'footer-input__notched-outline',
+        input: 'footer-input__input',
+      },
+    }}
+    InputLabelProps={{
+      classes: {
+        root: 'footer-input__label',
+      },
+    }}
+    label={label}
+    value={value}
+    onChange={onChange}
+    margin="dense"
+    variant="outlined"
+  />
+)
 
 export default Footer
